9adbe70cb9e9d3b3b4d4622bb7fd282607b63f73
Dayeon Kang <sgdy20@naver.com>
2021-10-06 13:31:11 +0900

    Create LoginView 1

--- FoodDetector/ContentView.swift
+++ FoodDetector/ContentView.swift
@@ -7,15 +7,103 @@
 
 import SwiftUI
 
+let storedUsername = "20161557"
+let storedPassword = "abcabc"
+
 struct ContentView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    @State var authenticationDidFail: Bool = false
+    @State var authenticationDidSucceed: Bool = true
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TitleText()
+            
+            HStack{
+                VStack{
+                    UsernameTextField(username: $username)
+                    PasswordSecureField(password: $password)
+                }
+                .padding()
+               
+                Button(action: {
+                    if self.username == storedUsername && self.password == storedPassword{
+                        self.authenticationDidSucceed = true
+                    } else {
+                        self.authenticationDidFail = true
+                    }
+                }) {
+                    LoginButtonContent()
+                }
+            }
+            
+            if authenticationDidFail {
+                Text("Information not correct. Try Again.")
+                    .offset(y: -10)
+                    .foregroundColor(.red)
+            }
+        }
+        .padding()
+        if authenticationDidSucceed {
+            Text("Login succeeded!")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
+    }
+}
+
+struct TitleText: View {
+    var body: some View {
+        Text("Food Detector!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct LoginButtonContent: View {
+    var body: some View {
+        Text("LOGIN")
+            .font(.body)
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 90, height:50)
+            .background(Color(#colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)))
+            .cornerRadius(20.0)
+    }
+}
+
+struct UsernameTextField: View {
+    
+    @Binding var username: String
+    
+    var body: some View {
+        TextField("Username", text: $username)
+            .padding()
+            .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
+
+struct PasswordSecureField: View {
+    
+    @Binding var password: String
+    
+    var body: some View {
+        SecureField("Password", text: $password)
+            .padding()
+            .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
     }
 }

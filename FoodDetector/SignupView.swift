c1692dbf5d0bcaba329476a1f684231cc1482b94
Dayeon Kang <sgdy20@naver.com>
2021-10-09 00:15:27 +0900

    Make SignUp View

---
+++ FoodDetector/SignupView.swift
@@ -0,0 +1,24 @@
+//
+//  SignupView.swift
+//  FoodDetector
+//
+//  Created by 강다연 on 2021/10/09.
+//
+
+import SwiftUI
+
+struct SignupView: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello")
+                .navigationTitle("SignUpView")
+                .navigationBarTitle("ViewControler")
+        }
+    }
+}
+
+struct SignupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignupView()
+    }
+}
